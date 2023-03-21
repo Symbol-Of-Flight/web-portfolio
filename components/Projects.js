55b3cbaee8c84910c72105ba28b055edc2e00892
Fu <hi@fufei.io>
2023-03-21 15:25:20 +0800

    added Daily steps

--- components/Projects.js
+++ components/Projects.js
@@ -32,6 +32,7 @@ const Projects = () => {
                 </div>
             </div>
         </div>
+
         <div className='flex flex-col-reverse lg:flex-row lg:mx-40 my-20 items-center'>
 
             <div className='text-left lg:-mr-12 z-10 pt-10 lg:pt-0'>
@@ -42,8 +43,8 @@ const Projects = () => {
                     Grateful Cloud
                 </div>
                 <div className='p-5 max-w-md bg-[#001D46] rounded-lg font-Inter text-[#6F7F9E] shadow-md '>
-                    A repository of design resources for UX/UI designers.
-                    Everything you need for your next design project, from design systems to free stock photos, from free illustrations to UI mockups.
+                    A repository of resources for non-profits
+                    From email marketing to team collaboration, these are a list of tech companies that offer discounts or free tiers.
                 </div>
                 <div className='text-left pr-6 mt-2'>
                     <a className='text-[#F50057]' href='https://grateful-cloud.fufei.io/' target='_blank' rel="noreferrer">
@@ -53,6 +54,29 @@ const Projects = () => {
             </div>
             <div className='lg:-ml-20 grayscale-[80%] hover:grayscale-0 hover:scale-110 transition duration-200 '>
                 <Image src="/GC.png" width={500} height={300} alt='Grateful Cloud'/>
+            </div>
+        </div>
+
+        <div className='flex flex-col lg:flex-row lg:mx-40 my-20 items-center'>
+            <div className='lg:-mr-20 grayscale-[80%] transition hover:grayscale-0 ease-in duration-200 hover:scale-110'>
+                <Image src="/DeC.png" width={500} height={300} alt='Daily Steps'/>
+            </div>
+            <div className='text-right lg:-ml-12 z-10 pt-10 lg:pt-0'>
+                <div className='text-[#F50057] font-Fira pr-3'>
+                    Productivity
+                </div>
+                <div className='text-2xl font-bold font-Inter pr-3'>
+                    Daily Steps
+                </div>
+                <div className='p-5 max-w-md bg-[#001D46] rounded-lg font-Inter text-[#6F7F9E] shadow-md '>
+                    A chrome extension that turns new tabs into a to-do list
+                    An easy way to keep track of today's task(s)
+                </div>
+                <div className='text-right pr-6 mt-2'>
+                    <a className='text-[#F50057]' href='https://dailysteps.co/' target='_blank' rel="noreferrer">
+                        Link
+                    </a>
+                </div>
             </div>
         </div>
     </div>
